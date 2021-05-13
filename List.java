05ab288544d07ab419a8e31208ce8c0250cb24c3
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-05-13 21:32:35 +0200

    MàJ

--- List.java
+++ List.java
@@ -1,12 +1,21 @@
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
+import java.awt.Component;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JPanel;
 import javax.swing.JOptionPane;
 import java.util.*;
+import java.awt.Component;
+import javax.swing.BoxLayout ;
 import javax.swing.ImageIcon;
 import javax.swing.AbstractAction;
 public class List extends AbstractAction {
 	private ToDoApp fenetre;
 	private String name ;
 	private String listElements ;
+	private JLabel nameLabel;
+  private JLabel listLabel;
   public List(ToDoApp fenetre, String texte){
 		super(texte);
 
@@ -17,6 +26,18 @@ public class List extends AbstractAction {
 			String listElements = JOptionPane.showInputDialog("List Elements (separated by comas) : ");
 			String[] elementsList = listElements.split(",");
 			int j = elementsList.length ;
-			System.out.println(j);
+
+			JPanel panel = new JPanel();
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			nameLabel = new JLabel(name.toUpperCase());
+			nameLabel.setBounds(50, 50, 100, 30);
+			nameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+			nameLabel.setFont(new Font("Monospaced", Font.PLAIN, 18));
+			panel.add(nameLabel);
+			listLabel = new JLabel("<html>" + elementsList[0] + "<br>" + elementsList[1] + "</html>");
+			listLabel.setBounds(50, 50, 100, 30);
+			listLabel.setFont(new Font("Monospaced", Font.PLAIN, 12));
+			panel.add(listLabel);
+			JOptionPane.showMessageDialog(null, panel, name,JOptionPane.DEFAULT_OPTION);
 	}
 }
