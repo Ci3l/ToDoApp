5786a9b4f3bad493be08f8ee164c290e6bc54406
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-05-15 14:38:37 +0200

    OptionPane to JFrame MàJ

--- PostIt.java
+++ PostIt.java
@@ -1,9 +1,12 @@
 import java.awt.Dimension;
+import javax.swing.BoxLayout ;
 import java.awt.event.ActionEvent;
+import java.awt.FlowLayout ;
 import java.awt.Font;
 import java.awt.Component;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.AbstractAction;
 import java.awt.BorderLayout;
@@ -21,57 +24,73 @@ public class PostIt {
   private static JLabel timeLabel;
   public static void PostIt(String name, String note, Object month, Object date, Object hour, Object minute, String strMonth, String strDate, String strHour, String strMinute){
     try {
-      ImageIcon icon = new ImageIcon(new URL("https://cdn.tutsplus.com/mac/authors/legacy/Josh%20Johnson/2012/09/26/Stickies1.png"));//https://upload.wikimedia.org/wikipedia/commons/4/43/Nuvola_apps_user_with_note.svg
-      JLabel label = new JLabel(icon);
-      JPanel panel = new JPanel(new GridBagLayout());
-      panel.add(label);
-      JPanel textPanel = new JPanel() ;
-      textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
-      nameLabel = new JLabel(name.toUpperCase());
-      nameLabel.setBounds(50, 50, 100, 30);
-      nameLabel.setFont(new Font("Monospaced", Font.PLAIN, 18));
-      textPanel.add(nameLabel,Component.CENTER_ALIGNMENT);
-      monthLabel = new JLabel(strMonth + ' ' + strDate);
-      monthLabel.setBounds(50, 50, 100, 30);
-      monthLabel.setFont(new Font("Monospaced", Font.PLAIN, 15));
-      textPanel.add(monthLabel,Component.CENTER_ALIGNMENT);
-      timeLabel = new JLabel(strHour + ':' + strMinute);
-      timeLabel.setBounds(50, 50, 100, 30);
-      timeLabel.setFont(new Font("Monospaced", Font.PLAIN, 14));
-      textPanel.add(timeLabel,Component.CENTER_ALIGNMENT);
-      noteLabel = new JLabel(note.toLowerCase());
-      noteLabel.setBounds(50, 50, 100, 30);
-      noteLabel.setFont(new Font("Monospaced", Font.PLAIN, 12));
-      textPanel.add(noteLabel,Component.CENTER_ALIGNMENT);
-      JPanel panel2 = new JPanel(new BorderLayout());
-      panel2.add(textPanel, BorderLayout.EAST);
-      panel2.add(panel, BorderLayout.WEST);
-      String[] options = {"done", "cancel", "modify"};
-      int retour = JOptionPane.showOptionDialog(null, panel2, name,
-              JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-      if (retour == 0){
-        int g = ToDoApp.getAccomplishment();
-        g ++ ;
-        ToDoApp.setAccomplishment(g);
-        if (g % 10 == 1){
-          JOptionPane.showMessageDialog(null,"It's your " + g + "st done post-it", "Accomplishment",
-          JOptionPane.INFORMATION_MESSAGE);}
-        else if (g % 10 == 2){
-          JOptionPane.showMessageDialog(null,"It's your " + g + "nd done post-it", "Accomplishment",
-          JOptionPane.INFORMATION_MESSAGE);}
-        else if (g % 10 == 3){
-          JOptionPane.showMessageDialog(null,"It's your " + g + "rd done post-it", "Accomplishment",
-          JOptionPane.INFORMATION_MESSAGE);}
-        else {
-          JOptionPane.showMessageDialog(null,"It's your " + g + "th done post-it", "Accomplishment",
-          JOptionPane.INFORMATION_MESSAGE);}}
-      else if (retour == 1){
-        //close;
-      }
-      else {
-        //JOptionPane for modification;
-      }
-      } catch ( Exception a){
-    }
-  }
+    JFrame postIt = new JFrame(name);
+    JPanel postItPanel = new JPanel();
+    postItPanel.setLayout(new BoxLayout(postItPanel, BoxLayout.Y_AXIS));
+
+    ImageIcon icon = new ImageIcon(new URL("https://cdn.tutsplus.com/mac/authors/legacy/Josh%20Johnson/2012/09/26/Stickies1.png"));
+    JLabel label = new JLabel(icon);
+    JPanel panel = new JPanel(new GridBagLayout());
+    panel.add(label);
+    JPanel textPanel = new JPanel() ;
+    textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+    nameLabel = new JLabel(name.toUpperCase());
+    nameLabel.setBounds(50, 50, 100, 30);
+    nameLabel.setFont(new Font("Monospaced", Font.PLAIN, 18));
+    textPanel.add(nameLabel,Component.CENTER_ALIGNMENT);
+    monthLabel = new JLabel(strMonth + ' ' + strDate);
+    monthLabel.setBounds(50, 50, 100, 30);
+    monthLabel.setFont(new Font("Monospaced", Font.PLAIN, 15));
+    textPanel.add(monthLabel,Component.CENTER_ALIGNMENT);
+    timeLabel = new JLabel(strHour + ':' + strMinute);
+    timeLabel.setBounds(50, 50, 100, 30);
+    timeLabel.setFont(new Font("Monospaced", Font.PLAIN, 14));
+    textPanel.add(timeLabel,Component.CENTER_ALIGNMENT);
+    noteLabel = new JLabel(note.toLowerCase());
+    noteLabel.setBounds(50, 50, 100, 30);
+    noteLabel.setFont(new Font("Monospaced", Font.PLAIN, 12));
+    textPanel.add(noteLabel,Component.CENTER_ALIGNMENT);
+    JPanel panel2 = new JPanel(new BorderLayout());
+    panel2.add(textPanel, BorderLayout.EAST);
+    panel2.add(panel, BorderLayout.WEST);
+
+    JPanel panel3 = new JPanel(new FlowLayout());
+    JButton buttonDone = new JButton();
+    buttonDone.setText("done");
+    buttonDone.addActionListener(e -> {postIt.dispose();
+      int g = ToDoApp.getAccomplishment();
+  		g ++ ;
+  		ToDoApp.setAccomplishment(g);
+  		if (g % 10 == 1){
+  			JOptionPane.showMessageDialog(null,"It's your " + g + "st done post-it", "Accomplishment",
+  			JOptionPane.INFORMATION_MESSAGE);}
+  		else if (g % 10 == 2){
+  			JOptionPane.showMessageDialog(null,"It's your " + g + "nd done post-it", "Accomplishment",
+  			JOptionPane.INFORMATION_MESSAGE);}
+  		else if (g % 10 == 3){
+  			JOptionPane.showMessageDialog(null,"It's your " + g + "rd done post-it", "Accomplishment",
+  			JOptionPane.INFORMATION_MESSAGE);}
+  		else {
+  			JOptionPane.showMessageDialog(null,"It's your " + g + "th done post-it", "Accomplishment",
+  			JOptionPane.INFORMATION_MESSAGE);}
+    });
+    JButton buttonCancel = new JButton();
+    buttonCancel.setText("cancel");
+    buttonDone.addActionListener(e -> {postIt.dispose();});
+    JButton buttonModify = new JButton(new ModifyPostIt(postIt, "modify"));
+    panel3.add(buttonDone);
+    panel3.add(buttonCancel);
+    panel3.add(buttonModify);
+
+
+    postItPanel.add(panel2);
+    postItPanel.add(panel3);
+
+    postIt.add(postItPanel);
+    postIt.setSize(300, 300);
+    postIt.setLocationRelativeTo(null);
+    postIt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    postIt.setVisible(true);
+  } catch ( Exception a){}//https://upload.wikimedia.org/wikipedia/commons/4/43/Nuvola_apps_user_with_note.svg
+}
 }
