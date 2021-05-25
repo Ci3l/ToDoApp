dfa586a17365962569d870244af61c24d27961f9
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-05-25 18:41:42 +0200

    MàJ

--- Checklist.java
+++ Checklist.java
@@ -1,17 +1,13 @@
+import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.Component;
-import java.util.ArrayList;
 import javax.swing.JLabel;
-import java.awt.Font;
 import javax.swing.JPanel;
 import javax.swing.JOptionPane;
-import java.awt.event.ActionEvent;
-import java.util.*;
-import java.awt.Component;
 import javax.swing.BoxLayout ;
 import javax.swing.ImageIcon;
 import javax.swing.AbstractAction;
@@ -22,33 +18,27 @@ public class Checklist {
     private static JLabel listLabel;
     public static void Checklist() {
         String name = JOptionPane.showInputDialog("Name : ");
-
         JFrame checklist = new JFrame(name);
         JPanel checklistPanel = new JPanel();
         checklistPanel.setLayout(new BoxLayout(checklistPanel, BoxLayout.Y_AXIS));
-
 			  String listElements = JOptionPane.showInputDialog("List Elements (separated by comas) : ");
         String[] elementsList = listElements.split(",") ;
   			ArrayList<String> elementsListArray = new ArrayList<String>(20) ;
   			for (String i : elementsList) { elementsListArray.add("-> " + i);}
   			int j = elementsListArray.size() ;
   			while(j <= 20) { elementsListArray.add(" "); j ++ ;}
-
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
         nameLabel = new JLabel(name.toUpperCase());
   			nameLabel.setBounds(50, 50, 100, 30);
   			nameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
   			nameLabel.setFont(new Font("Monospaced", Font.PLAIN, 18));
   			panel.add(nameLabel);
-
   			listLabel = new JLabel("<html>" + elementsListArray.get(0).toString() + "<br>" +  elementsListArray.get(1).toString() + "<br>" +  elementsListArray.get(2).toString() + "<br>" +  elementsListArray.get(3).toString() + "<br>" +  elementsListArray.get(4).toString() + "<br>" +  elementsListArray.get(5).toString() + "<br>" +  elementsListArray.get(6).toString() + "<br>" +  elementsListArray.get(7).toString() + "<br>" +  elementsListArray.get(8).toString() + "<br>" +  elementsListArray.get(9).toString() + "<br>" +  elementsListArray.get(10).toString() + "<br>" +  elementsListArray.get(11).toString() + "<br>" +  elementsListArray.get(12).toString() + "<br>" +  elementsListArray.get(13).toString() + "<br>" +  elementsListArray.get(14).toString() + "<br>" +  elementsListArray.get(15).toString() + "<br>" +  elementsListArray.get(16).toString() + "<br>" +  elementsListArray.get(17).toString() + "<br>" +  elementsListArray.get(18).toString() + "<br>" +  elementsListArray.get(19).toString() + "<br>" +  elementsListArray.get(20).toString() + "</html>");
   			listLabel.setBounds(50, 50, 100, 30);
         listLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
   			listLabel.setFont(new Font("Monospaced", Font.PLAIN, 12));
   			panel.add(listLabel);
-
         JPanel panel2 = new JPanel(new FlowLayout());
         JButton buttonDone = new JButton();
         buttonDone.addActionListener(e -> {checklist.dispose();
@@ -56,17 +46,13 @@ public class Checklist {
             g ++ ;
             ToDoApp.setAccomplishmentForList(g);
             if (g % 10 == 1){
-              JOptionPane.showMessageDialog(null,"It's your " + g + "st done list", "Accomplishment",
-              JOptionPane.INFORMATION_MESSAGE);}
+              JOptionPane.showMessageDialog(null,"It's your " + g + "st done list", "Accomplishment",JOptionPane.INFORMATION_MESSAGE);}
             else if (g % 10 == 2){
-              JOptionPane.showMessageDialog(null,"It's your " + g + "nd done list", "Accomplishment",
-              JOptionPane.INFORMATION_MESSAGE);}
+              JOptionPane.showMessageDialog(null,"It's your " + g + "nd done list", "Accomplishment",JOptionPane.INFORMATION_MESSAGE);}
             else if (g % 10 == 3){
-              JOptionPane.showMessageDialog(null,"It's your " + g + "rd done list", "Accomplishment",
-              JOptionPane.INFORMATION_MESSAGE);}
+              JOptionPane.showMessageDialog(null,"It's your " + g + "rd done list", "Accomplishment",JOptionPane.INFORMATION_MESSAGE);}
             else {
-              JOptionPane.showMessageDialog(null,"It's your " + g + "th done list", "Accomplishment",
-              JOptionPane.INFORMATION_MESSAGE);} });
+              JOptionPane.showMessageDialog(null,"It's your " + g + "th done list", "Accomplishment",JOptionPane.INFORMATION_MESSAGE);} });
         buttonDone.setText("done");
         JButton buttonCancel = new JButton();
         buttonCancel.setText("cancel");
@@ -75,10 +61,8 @@ public class Checklist {
         panel2.add(buttonDone);
         panel2.add(buttonCancel);
         panel2.add(buttonModify);
-
         checklistPanel.add(panel);
         checklistPanel.add(panel2);
-
         checklist.add(checklistPanel);
         checklist.setSize(200, 470);
         checklist.setLocationRelativeTo(null);
--- PostIt.java
+++ PostIt.java
@@ -1,5 +1,3 @@
-import java.awt.Dimension;
-import javax.swing.BoxLayout ;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout ;
 import java.awt.Font;
@@ -12,7 +10,6 @@ import javax.swing.AbstractAction;
 import java.awt.BorderLayout;
 import javax.swing.BoxLayout ;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -35,25 +32,20 @@ public class PostIt {
     JPanel textPanel = new JPanel() ;
     textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
     nameLabel = new JLabel(name.toUpperCase());
-    //nameLabel.setBounds(50, 50, 100, 30);
     nameLabel.setFont(new Font("Monospaced", Font.PLAIN, 18));
     textPanel.add(nameLabel,Component.CENTER_ALIGNMENT);
     monthLabel = new JLabel(strMonth + ' ' + strDate + ' ');
-    //monthLabel.setBounds(50, 50, 100, 30);
     monthLabel.setFont(new Font("Monospaced", Font.PLAIN, 15));
     textPanel.add(monthLabel,Component.CENTER_ALIGNMENT);
     timeLabel = new JLabel(strHour + ':' + strMinute);
-    //timeLabel.setBounds(50, 50, 100, 30);
     timeLabel.setFont(new Font("Monospaced", Font.PLAIN, 14));
     textPanel.add(timeLabel,Component.CENTER_ALIGNMENT);
     noteLabel = new JLabel(note.toLowerCase() + ' ');
-    //noteLabel.setBounds(50, 50, 400, 30);
     noteLabel.setFont(new Font("Monospaced", Font.PLAIN, 12));
     textPanel.add(noteLabel,Component.CENTER_ALIGNMENT);
     JPanel panel2 = new JPanel(new BorderLayout());
     panel2.add(textPanel, BorderLayout.EAST);
     panel2.add(panel, BorderLayout.WEST);
-
     JPanel panel3 = new JPanel(new FlowLayout());
     JButton buttonDone = new JButton();
     buttonDone.setText("done");
@@ -62,17 +54,13 @@ public class PostIt {
   		g ++ ;
   		ToDoApp.setAccomplishment(g);
   		if (g % 10 == 1){
-  			JOptionPane.showMessageDialog(null,"It's your " + g + "st done post-it", "Accomplishment",
-  			JOptionPane.INFORMATION_MESSAGE);}
+  			JOptionPane.showMessageDialog(null,"It's your " + g + "st done post-it", "Accomplishment",JOptionPane.INFORMATION_MESSAGE);}
   		else if (g % 10 == 2){
-  			JOptionPane.showMessageDialog(null,"It's your " + g + "nd done post-it", "Accomplishment",
-  			JOptionPane.INFORMATION_MESSAGE);}
+  			JOptionPane.showMessageDialog(null,"It's your " + g + "nd done post-it", "Accomplishment",JOptionPane.INFORMATION_MESSAGE);}
   		else if (g % 10 == 3){
-  			JOptionPane.showMessageDialog(null,"It's your " + g + "rd done post-it", "Accomplishment",
-  			JOptionPane.INFORMATION_MESSAGE);}
+  			JOptionPane.showMessageDialog(null,"It's your " + g + "rd done post-it", "Accomplishment",JOptionPane.INFORMATION_MESSAGE);}
   		else {
-  			JOptionPane.showMessageDialog(null,"It's your " + g + "th done post-it", "Accomplishment",
-  			JOptionPane.INFORMATION_MESSAGE);}
+  			JOptionPane.showMessageDialog(null,"It's your " + g + "th done post-it", "Accomplishment",JOptionPane.INFORMATION_MESSAGE);}
     });
     JButton buttonCancel = new JButton();
     buttonCancel.setText("cancel");
@@ -81,17 +69,13 @@ public class PostIt {
     panel3.add(buttonDone);
     panel3.add(buttonCancel);
     panel3.add(buttonModify);
-
-
     postItPanel.add(panel2);
     postItPanel.add(panel3);
-
-
     postIt.add(postItPanel);
     int noteLength = note.length();
     System.out.println(noteLength);
-    if (noteLength <= 11){postIt.setSize(300, 300);}
-    else {
+    if (noteLength <= 11){postIt.setSize(300, 300);
+    } else {
       double newLengthForPostItDouble = (300 + (noteLength - 11 )*10)*(1 -0.17);
       int newLengthForPostItInt = (int)newLengthForPostItDouble;
       postIt.setSize(newLengthForPostItInt, 300);
@@ -100,10 +84,10 @@ public class PostIt {
     postIt.setLocationRelativeTo(null);
     postIt.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     postIt.setVisible(true);
-  } catch ( Exception a){}//https://upload.wikimedia.org/wikipedia/commons/4/43/Nuvola_apps_user_with_note.svg
+  } catch ( Exception a){}
 }
 public static void setName(JFrame frame,String newName){nameLabel.setText(newName.toUpperCase());}
 public static void setDate(JFrame frame,String newMonth, String newDate){monthLabel.setText(newMonth + ' ' + newDate + ' ');}
 public static void setTime(JFrame frame,String newHour,String newMinute){timeLabel.setText(newHour + ':' + newMinute);}
-public static void setNote(JFrame frame,String newNote){timeLabel.setText(noteLabel.toLowerCase() + ' ');}
+public static void setNote(JFrame frame,String newNote){timeLabel.setText(newNote.toLowerCase() + ' ');}
 }
