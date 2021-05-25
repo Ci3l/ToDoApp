1f53c50b1a458a8a9fc494096f2767e58c2efc33
Ci3l <80896731+Ci3l@users.noreply.github.com>
2021-05-25 18:36:30 +0200

    MàJ

--- ModifyChecklist.java
+++ ModifyChecklist.java
@@ -1,28 +1,14 @@
 import java.util.ArrayList;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
-import java.awt.Font;
-import java.awt.Component;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 import javax.swing.AbstractAction;
-import java.awt.BorderLayout;
-import javax.swing.BoxLayout ;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.net.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPanel;
 public class ModifyChecklist extends AbstractAction{
   private JFrame frame;
   public ModifyChecklist(JFrame frame, String texte){
     super(texte);
     this.frame = frame;
   }
-
   public void actionPerformed(ActionEvent e) {
     String[] options = {"name", "list"};
     int retour = JOptionPane.showOptionDialog(null, "What would you modify ?", "Modification",JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
--- ModifyPostIt.java
+++ ModifyPostIt.java
@@ -1,30 +1,16 @@
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
-import java.awt.Font;
-import java.awt.Component;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 import javax.swing.AbstractAction;
-import java.awt.BorderLayout;
-import javax.swing.BoxLayout ;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.net.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 public class ModifyPostIt extends AbstractAction{
   private JFrame frame;
   public ModifyPostIt(JFrame frame, String texte){
     super(texte);
     this.frame = frame;
   }
-
   public void actionPerformed(ActionEvent e) {
     String[] options = {"name", "date", "note"};
-    int retour = JOptionPane.showOptionDialog(null, "What would you modify ?", "Modification",
-            JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+    int retour = JOptionPane.showOptionDialog(null, "What would you modify ?", "Modification",JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
     if (retour == 0){
       String newName = JOptionPane.showInputDialog("Enter the new name");
       PostIt.setName(frame, newName);
@@ -37,8 +23,7 @@ public class ModifyPostIt extends AbstractAction{
          String[] dateList = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"} ;
          String date = (String)JOptionPane.showInputDialog(null,"Date Selection","New Date", JOptionPane.QUESTION_MESSAGE,null,dateList, dateList[0]);
          PostIt.setDate(frame,month,date);
-          }
-        else {
+          } else {
           String[] hourList = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"} ;
           String hour = (String)JOptionPane.showInputDialog(null,"Hour Selection","New Hour", JOptionPane.QUESTION_MESSAGE,null,hourList, hourList[0]);
           String[] minuteList = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"} ;
